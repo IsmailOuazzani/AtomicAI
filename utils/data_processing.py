841dfa2e5ed0a232b593af6d00874db928036b87
IsmailOuazzani <ismail.ouazzani@mail.utoronto.ca>
2023-03-11 01:50:44 -0500

    Update data_processing.py

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -44,7 +44,7 @@ class Board():
         else:
             self.board = board
 
-        self.board_map = np.zeros((8, 8, 19), dtype=np.uint8)
+        self.board_map = np.zeros((8, 8, 18))
         self.board_map = self.get_board_map()
 
     def get_board_map(self):
@@ -63,24 +63,26 @@ class Board():
         """
         # castling rights
         if self.board.has_kingside_castling_rights(chess.WHITE):
-            self.board_map[:,:,13] = 1
+            self.board_map[:,:,12] = 1
         if self.board.has_queenside_castling_rights(chess.WHITE):
-            self.board_map[:,:,14] = 1
+            self.board_map[:,:,13] = 1
         if self.board.has_kingside_castling_rights(chess.BLACK):
-            self.board_map[:,:,15] = 1
+            self.board_map[:,:,14] = 1
         if self.board.has_queenside_castling_rights(chess.BLACK):
-            self.board_map[:,:,16] = 1
+            self.board_map[:,:,15] = 1
         # en passant
         if self.board.ep_square: 
             ep = chess.square_file(self.board.ep_square)
-            self.board_map[:,ep,17] = 1
+            self.board_map[:,ep,16] = 1
 
-        self.board_map[:,:,18] = self.result # label to be predicted
+        self.board_map[:,:,17] = self.result # label to be predicted
 
         return self.board_map
 
 def get_boards(pgn_files):
     boards = []
+    data_counter = 0
+    file_counter = 0
     for pgn in pgn_files:
 
         # handle multiple games in one pgn file
@@ -105,22 +107,39 @@ def get_boards(pgn_files):
             game = chess.pgn.read_game(io.StringIO(cur_game))
             # print(cur_game)
             if game.headers['Result'] == '1-0' or game.headers['Result'] == '0-1' or game.headers['Result'] == '1/2-1/2':
-                if int(game.headers['WhiteElo']) > 1500:
+                #  check if white player is rated
+                if 'WhiteElo' in game.headers and int(game.headers['WhiteElo']) > 1500:
                     board = game.board()
                     i = 0
                     for move in game.mainline_moves():
                         board.push(move)
                         boards.append(Board(board, game.headers['Result']))
+                        data_counter += 1
                         # # save image of board, extremely slow!!
                         # svg = chess.svg.board(board=boards[-1].board)
                         # with open('utils/trash/'+'board' + str(i) + '.svg', 'w+') as f:
                         #     f.write(svg)
                         # i += 1
-    return boards
+            while(len(boards) > 1000):
+                # pop off the first 100 boards
+                temp_boards = [board.board_map for board in boards[:1000]]
+                boards = boards[1000:]
+
+                # save the first 100 boards to a file
+                np.save(PATH + str(file_counter*1000) + '.npy', temp_boards)
+                file_counter += 1
+            
+            # only save the first 20 000 games
+            if data_counter == 19999:
+                break
+    # pop off the first 1000 boards
+    temp_boards = [board.board_map for board in boards]
+    np.save(PATH + str(file_counter*1000) + '.npy', temp_boards)
+    return boards, data_counter
 
 
 if __name__ == '__main__':
-    boards = get_boards(get_pgn_files(PATH))
-    print(len(boards))
-    print(boards[1].board_map[:,:,0])
+    boards, dat = get_boards(get_pgn_files(PATH))
+    print( dat)
+    # print(boards[0].board_map[:,:,17])
     
